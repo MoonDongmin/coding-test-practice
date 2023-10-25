cbd12bb1a13611ac5aba8b14be1abbbead869d0e
MoonDongmin <20191836@o365.hanbat.ac.kr>
2023-10-25 11:39:53 +0900

    코딩 테스트 연습중...

---
+++ section01/세수중최솟값.ts
@@ -0,0 +1,27 @@
+function solution(a, b, c) {
+    let answer;
+
+    if (a < b && a < c) {
+        answer = a;
+    } else if (b < a && b < c) {
+        answer = b;
+    } else {
+        answer = c;
+    }
+    return answer;
+}
+
+console.log(solution(10, 1, 21));
+
+
+function mainSolution(a, b, c) {
+    let answer;
+
+    if (a < b) answer = a;
+    else answer = b;
+    if (c < answer) answer = c;
+
+    return answer;
+}
+
+console.log(mainSolution(6, 5, 11));
