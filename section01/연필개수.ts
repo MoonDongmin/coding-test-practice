d4e112a4ad957cae0e7e5516062d4b00f7915675
MoonDongmin <20191836@o365.hanbat.ac.kr>
2023-10-27 22:19:05 +0900

    연필개수

---
+++ section01/연필개수.ts
@@ -0,0 +1,12 @@
+function solution(n) {
+    let answer;
+    answer = Math.ceil(n / 12);
+    return answer;
+}
+
+console.log(solution(25));
+
+function mainSolution(n){
+    let answer;
+    return answer;
+}
