925b62c00382d6177dd1a2b2b2003952a49311c2
MoonDongmin <20191836@o365.hanbat.ac.kr>
2023-11-01 18:21:22 +0900

    대문자 찾기

---
+++ section01/대문자찾기.ts
@@ -0,0 +1,23 @@
+function solution(s) {
+    let answer = 0;
+    for (let x of s.toString()) {
+        if (x === x.toUpperCase()) {
+            answer++;
+        }
+    }
+    return answer;
+}
+
+let str = "KoreaTimeGood";
+console.log(solution(str));
+
+function mainSolution(s){
+    let answer=0;
+    for(let x of s){
+        // let num=x.charCodeAt();
+        // if(num>=65 && num<=90) answer++;
+        if(x===x.toUpperCase()) answer++;
+    }
+
+    return answer;
+}
