64c352e335e6b7cb04eb66add12c1e8cd054f682
MoonDongmin <20191836@o365.hanbat.ac.kr>
2023-11-21 14:30:20 +0900

    점수계산

---
+++ section02/점수계산.js
@@ -0,0 +1,26 @@
+function solution(arr) {
+    let answer = 0, cnt = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 1) {
+            cnt++;
+            answer += cnt;
+        } else cnt = 0;
+    }
+    return answer;
+}
+
+function mainSolution(arr) {
+    let answer = 0, cnt = 0;
+
+    for (let x of arr) {
+        if (x === 1) {
+            cnt++;
+            answer += cnt;
+        } else cnt = 0;
+    }
+    return answer;
+}
+
+let arr = [1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
+console.log(solution(arr));
