7a6a05cdb6f2628d77aae4b797e7e815994e7ac7
MoonDongmin <20191836@o365.hanbat.ac.kr>
2024-02-29 18:55:37 +0900

    공주 구하기

--- section06/공주구하기.js
+++ section06/공주구하기.js
@@ -1,8 +1,10 @@
 function solution(n, k) {
     let answer;
+    // (배열의 길이, (v,i)) => v는 value 값, i = 인덱스 값
     let queue = Array.from({length: n}, (v, i) => i + 1);
 
-    while(queue.length){
+
+    while(queue.length){ // 큐가 하나일 때 까지 반복을 함
         for(let i=1; i<k; i++) queue.push(queue.shift());
         queue.shift();
         if(queue.length===1) answer=queue.shift();
