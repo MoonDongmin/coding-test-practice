76f4b8fb9640a2789a1d68bd060faba0567e0da6
MoonDongmin <20191836@o365.hanbat.ac.kr>
2024-03-08 17:18:40 +0900

    버블 정렬

---
+++ section07/버블정렬.js
@@ -0,0 +1,26 @@
+function solution(arr) {
+    let answer = arr;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            let idx = j;
+            if (arr[i] < arr[idx]) idx = i;
+            [arr[i], arr[idx]] = [arr[idx], arr[i]];
+        }
+    }
+    return answer;
+}
+
+function mainSolution(arr) {
+    let answer = arr;
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+    }
+    return answer;
+}
+
+let arr = [13, 5, 11, 7, 23, 15];
+console.log(mainSolution(arr));
